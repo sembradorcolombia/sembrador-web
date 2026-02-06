8fcb514fdf0a3ba3a829cab2970755a33e9f25de
Jorge Ossa <sembradorcdelrio@gmail.com>
2026-02-06 00:07:07 -0500

    feat(ui): add equilibrio logo

--- src/routes/equilibrio.tsx
+++ src/routes/equilibrio.tsx
@@ -1,9 +1,23 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { LogoEquilibrio } from "@/components/LogoEquilibrio";
 
-export const Route = createFileRoute('/equilibrio')({
+export const Route = createFileRoute("/equilibrio")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/equilibrio"!</div>
+  return (
+    <main className="bg-secondary w-full h-screen">
+      <section className="wrapper max-w-300 m-auto">
+        <article>
+          <header>
+            <img src="/logo-hw.svg" alt="logo el sembrador" className="h-20" />
+            <LogoEquilibrio decoratorColor="primary" />
+          </header>
+          <section>Content</section>
+          <footer>Footer</footer>
+        </article>
+      </section>
+    </main>
+  );
 }
